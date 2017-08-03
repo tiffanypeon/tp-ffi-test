2203ab92606acedd38f5f5ae1be5937e56b7eb36
Scott Feinberg <scott.feinberg@nytimes.com>
2017-08-03 15:45:17 -0400

    switch to using a submodule

--- ext/nytscracker/nytscracker.go
+++ ext/nytscracker/nytscracker.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 
 	nytcookie "github.com/nytm/go-nytcookie"
 )
@@ -15,6 +16,15 @@ func crackRaw(data *C.char) *C.char {
 	client, err := nytcookie.NewClient(os.Getenv("NYTS_SECRET"), "")
 	if err != nil {
 		fmt.Printf("Error creating client %v", err)
+	}
+
+	// Use `os.Environ` to list all key/value pairs in the
+	// environment. This returns a slice of strings in the
+	// form `KEY=value`. You can `strings.Split` them to
+	// get the key and value. Here we print all the keys.
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0])
 	}
 
 	cookie, err := client.ParseS(nyts)
